42b35d9717f1edcec6d4257a87a3bb301e946254
Dmitry Ryazancev <40-ryazancev_dl@users.noreply.gitlab.radar076.ru>
2024-02-18 13:46:34 +0300

    Update proxy.h

--- include/leafconf/proxy.h
+++ include/leafconf/proxy.h
@@ -1,5 +1,66 @@
 #pragma once
 
+#include <functional>
+#include <leaf/math.h>
+
 namespace leaf::conf
 {
+  using std::function;
+
+  template<typename T>
+  class Proxy
+  {
+    public:
+      explicit Proxy(T val, function<void (const T&)> on_change);
+      Proxy(const Proxy& other) = delete;
+      Proxy(const Proxy&& other) = delete;
+
+      [[nodiscard]] auto get() const -> const T&;
+      [[nodiscard]] auto operator*() const -> const T&;
+
+      auto set(const T& val) -> void;
+
+      auto operator=(const Proxy& other) -> Proxy&;
+      auto operator=(Proxy&& other) noexcept -> Proxy&;
+
+    private:
+      T val;
+      function<void (const T&)> changed;
+  };
+
+  template<typename T>
+  Proxy<T>::Proxy(T val, function<void(const T &)> on_change)
+    : val(val),
+      changed(on_change)
+  {}
+
+  template<typename T>
+  auto Proxy<T>::get() const -> const T & { return this->val; }
+
+  template<typename T>
+  auto Proxy<T>::operator*() const -> const T & { return this->get(); }
+
+  template <typename T>
+  auto Proxy<T>::set(const T& val) -> void
+  {
+    if(eq(val, this->val))
+      return;
+    this->val = val;
+    if(this->changed)
+      this->changed(this->val);
+  }
+
+  template<typename T>
+  auto Proxy<T>::operator=(const Proxy& other) -> Proxy&
+  {
+    this->set(other.get());
+    return *this;
+  }
+
+  template<typename T>
+  auto Proxy<T>::operator=(Proxy&& other) noexcept -> Proxy&
+  {
+    this->set(other.get());
+    return *this;
+  }
 }
